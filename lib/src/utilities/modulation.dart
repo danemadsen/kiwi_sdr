615f4f2da17706329366aac449b000402793eb6e
danemadsen <dane_madsen@hotmail.com>
2025-05-09 16:01:44 +1000

    mode

--- lib/src/utilities/modulation.dart
+++ lib/src/utilities/modulation.dart
@@ -1,24 +1,29 @@
 part of 'package:flutter_sdr/flutter_sdr.dart';
 
 enum Modulation {
-  am,
-  amn,
-  amw,
-  sam,
-  sal,
-  sau,
-  sas,
-  qam,
-  drm,
-  lsb,
-  lsn,
-  usb,
-  usn,
-  cw,
-  cwn,
-  nbfm,
-  nnfm,
-  iq;
+  am(-4900, 4900),
+  amn(-2500, 2500),
+  amw(-6000, 6000),
+  sam(-4900, 4900),
+  sal(-4900, 0),
+  sau(0, 4900),
+  sas(-4900, 4900),
+  qam(-4900, 4900),
+  drm(-5000, 5000),
+  lsb(-2700, -300),
+  lsn(-2400, -300),
+  usb(300, 2700),
+  usn(300, 2400),
+  cw(300, 700),
+  cwn(470, 530),
+  nbfm(-6000, 6000),
+  nnfm(-3000, 3000),
+  iq(-5000, 5000);
+
+  final int lc;
+  final int hc;
+
+  const Modulation(this.lc, this.hc);
 
   static Modulation fromString(String str) {
     switch (str) {
@@ -60,88 +65,6 @@ enum Modulation {
         return Modulation.iq;
       default:
         throw ArgumentError('Unknown modulation type: $str');
-    }
-  }
-
-  int get lc {
-    switch (this) {
-      case Modulation.am:
-        return -4900;
-      case Modulation.amn:
-        return -2500;
-      case Modulation.amw:
-        return -6000;
-      case Modulation.sam:
-        return -4900;
-      case Modulation.sal:
-        return -4900;
-      case Modulation.sau:
-        return 0;
-      case Modulation.sas:
-        return -4900;
-      case Modulation.qam:
-        return -4900;
-      case Modulation.drm:
-        return -5000;
-      case Modulation.lsb:
-        return -2700;
-      case Modulation.lsn:
-        return -2400;
-      case Modulation.usb:
-        return 300;
-      case Modulation.usn:
-        return 300;
-      case Modulation.cw:
-        return 300;
-      case Modulation.cwn:
-        return 470;
-      case Modulation.nbfm:
-        return -6000;
-      case Modulation.nnfm:
-        return -3000;
-      case Modulation.iq:
-        return -5000;
-    }
-  }
-
-  int get hc {
-    switch (this) {
-      case Modulation.am:
-        return 4900;
-      case Modulation.amn:
-        return 2500;
-      case Modulation.amw:
-        return 6000;
-      case Modulation.sam:
-        return 4900;
-      case Modulation.sal:
-        return 0;
-      case Modulation.sau:
-        return 4900;
-      case Modulation.sas:
-        return 4900;
-      case Modulation.qam:
-        return 4900;
-      case Modulation.drm:
-        return 5000;
-      case Modulation.lsb:
-        return -300;
-      case Modulation.lsn:
-        return -300;
-      case Modulation.usb:
-        return 2700;
-      case Modulation.usn:
-        return 2400;
-      case Modulation.cw:
-        return 700;
-      case Modulation.cwn:
-        return 530;
-      case Modulation.nbfm:
-        return 6000;
-      case Modulation.nnfm:
-        return 3000;
-      case Modulation.iq:
-        return 5000;
     }
   }
 }
